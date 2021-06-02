39b777b40fbd86fa66be0c314b86866179ec7b59
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-06-02 20:02:47 +0200

    refact: read user input into UserData struct

--- src/main.rs
+++ src/main.rs
@@ -13,58 +13,85 @@ struct WindowDate {
     end: String,
     month: String,
 }
-fn main() {
-    const DATE_FORMAT: &str = "%Y-%m-%d";
 
-    let last_date_input: String = Input::new()
-        .validate_with(|input: &String| -> Result<(), &str> {
-            let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-            if re.is_match(input) {
-                Ok(())
-            } else {
-                Err("This is not valid date")
-            }
-        })
-        .with_prompt("Enter the first day of your last period (YYYY-MM-DD)")
-        .with_initial_text(Utc::now().format(DATE_FORMAT).to_string())
-        .default("No".into())
-        .interact_text()
-        .unwrap();
+struct UserData<T> {
+    last_period_date: NaiveDate,
+    avg_cycle_days: T,
+}
 
-    let avg_cycle_days_input: String = Input::new()
+struct Rustovuli {}
+const DATE_FORMAT: &str = "%Y-%m-%d";
+
+fn fetch_user_data() -> Result<UserData<u16>, String> {
+    let today = Utc::now().format(DATE_FORMAT).to_string();
+    let default_avg_cycle_days = String::from("25");
+
+    let last_date_input = NaiveDate::parse_from_str(
+        &Input::new()
+            .validate_with(|input: &String| -> Result<(), &str> {
+                let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+                if re.is_match(input) {
+                    Ok(())
+                } else {
+                    Err("This is not valid date")
+                }
+            })
+            .with_prompt("Enter the first day of your last period (YYYY-MM-DD)")
+            .with_initial_text(&today)
+            .default(today.as_str().into())
+            .interact_text()
+            .unwrap(),
+        DATE_FORMAT,
+    )
+    .unwrap();
+
+    let avg_cycle_days_input = Input::new()
         .validate_with(|input: &String| -> Result<(), &str> {
-            match input.parse::<u32>() {
+            match input.parse::<u16>() {
                 Ok(_v) => Ok(()),
                 Err(_e) => Err("It must be a number"),
             }
         })
         .with_prompt("How long is your average cycle (in Days)?")
-        .default(String::from("25"))
+        .default(default_avg_cycle_days)
         .interact_text()
+        .unwrap()
+        .parse::<u16>()
         .unwrap();
 
-    let last_date = NaiveDate::parse_from_str(&last_date_input, DATE_FORMAT).unwrap();
-    let avg_cycle_days = avg_cycle_days_input.parse().unwrap();
+    let user_data = UserData {
+        last_period_date: last_date_input,
+        avg_cycle_days: avg_cycle_days_input,
+    };
+
+    Ok(user_data)
+}
+
+fn main() -> Result<(), String> {
+    let user_data = fetch_user_data().expect("Failed to read your input");
 
     // compute next period
-    let next_period_date = last_date + Duration::days(avg_cycle_days - 1);
+    let next_period_date =
+        user_data.last_period_date + Duration::days(user_data.avg_cycle_days as i64 - 1);
     let next_period = SimpleDate {
         day: next_period_date.format("%d").to_string(),
         month: next_period_date.format("%b").to_string(),
     };
 
     // compute next pregnancy test date
-    let next_pregnancy_test_date = last_date + Duration::days(avg_cycle_days);
+    let next_pregnancy_test_date =
+        user_data.last_period_date + Duration::days(user_data.avg_cycle_days as i64);
     let next_pregnancy_test = SimpleDate {
         day: next_pregnancy_test_date.format("%d").to_string(),
         month: next_pregnancy_test_date.format("%b").to_string(),
     };
 
     // compute ovulation date
-    const MAX_OVULATION_DAYS: i64 = 26;
-    let current_pregnancy_cycle = 40 - avg_cycle_days;
+    const MAX_OVULATION_DAYS: u16 = 26;
+    let current_pregnancy_cycle = 40 - user_data.avg_cycle_days;
     let ovulation_days = MAX_OVULATION_DAYS - current_pregnancy_cycle - 1;
-    let approximate_ovulation_date = last_date + Duration::days(ovulation_days);
+    let approximate_ovulation_date =
+        user_data.last_period_date + Duration::days(ovulation_days as i64);
     let approximate_ovulation = SimpleDate {
         day: approximate_ovulation_date.format("%d").to_string(),
         month: approximate_ovulation_date.format("%b").to_string(),
@@ -123,4 +150,8 @@ fn main() {
         .border_color(billboard::BorderColor::Yellow)
         .build()
         .display(&result);
+    // // return a new error from main that said what happened!
+    // return Err(String::from("something went wrong in main!"));
+
+    Ok(())
 }
